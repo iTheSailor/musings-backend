ecd0c44ad17f7d08a0b26740a3db135d4abf3898
iTheSailor <112807438+iTheSailor@users.noreply.github.com>
2024-06-26 17:31:17 -0400

    dev commit 1

--- finance/models.py
+++ finance/models.py
@@ -14,3 +14,41 @@ class UserWatchlist(models.Model):
     
     class Meta:
         unique_together = ('user', 'symbol')
+
+class UserWallet(models.Model):
+    #give user 100 thousand dollars to start with, dont allow negative balance
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=100000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+class UserTransaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=10)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.symbol
+    
+    class Meta:
+        unique_together = ('user', 'symbol', 'transaction_type')
+
+class UserPortfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=10)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.symbol
+    
+    class Meta:
+        unique_together = ('user', 'symbol')
